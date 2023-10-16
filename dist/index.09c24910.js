3e11f738a6dabc490fe21c2f1a2ccdfc66924c5f
Ropepe <pericavrhovac@gmail.com>
2023-10-16 04:26:09 +0200

    final project

---
+++ dist/index.09c24910.js
@@ -0,0 +1,57 @@
+function readFirst() {
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+    if (moreText.style.display === "none") {
+        moreText.style.display = "block";
+        btnText.innerHTML = "-";
+    } else {
+        moreText.style.display = "none";
+        btnText.innerHTML = "+";
+    }
+}
+function readSecond() {
+    var moreText = document.getElementById("more2");
+    var btnText = document.getElementById("myBtn2");
+    if (moreText.style.display === "none") {
+        moreText.style.display = "block";
+        btnText.innerHTML = "-";
+    } else {
+        moreText.style.display = "none";
+        btnText.innerHTML = "+";
+    }
+}
+function readThird() {
+    var moreText = document.getElementById("more3");
+    var btnText = document.getElementById("myBtn3");
+    if (moreText.style.display === "none") {
+        moreText.style.display = "block";
+        btnText.innerHTML = "-";
+    } else {
+        moreText.style.display = "none";
+        btnText.innerHTML = "+";
+    }
+}
+function addToCart() {
+    const element = document.getElementById("cartIcon");
+// element.classList.remove('classname'); // reset animation
+// void element.offsetWidth; // trigger reflow
+// element.classList.add('classname'); // start animation
+}
+function increase() {
+    var input = document.getElementById("quantity_number");
+    input.value = parseInt(input.value) + 1;
+}
+function decrease() {
+    var input = document.getElementById("quantity_number");
+    if (input.value > 1) input.value = parseInt(input.value) - 1;
+}
+function openMenu() {
+    var menu = document.getElementById("menu_mobile");
+    menu.style.display = "block";
+}
+function closeMenu() {
+    var menu = document.getElementById("menu_mobile");
+    menu.style.display = "none";
+}
+
+//# sourceMappingURL=index.09c24910.js.map
